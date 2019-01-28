096af5ed708914278f6370e73bb02e72f15d8312
unknown <clement.thomas@epitech.eu>
2019-01-28 09:53:29 +0100

    gitignore

---
+++ EpiBubble/Assets/Scripts/Mover.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public GameObject arrowRotation;
+    //float bubbleRadius = 0.5f;
+    //float rotationZ;
+    private Vector3 position;
+    private Vector3 mooving;
+    public Rigidbody2D rb;
+
+    private bool wallTouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallTouch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wallTouch == false)
+        {
+            position = transform.position;
+            mooving = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+
+            position += transform.rotation * mooving;
+            transform.position = position;
+        }
+        else
+        {
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Wall_Left")
+        {
+            wallTouch = true;
+            print("Mur gauche touché");
+        }
+    }
+}
---
+++ EpiBubble/Assets/Scripts/ShooterControler.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterControler : MonoBehaviour
+{
+    private float rotationSpeed = 80.0f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.back * this.rotationSpeed * Time.deltaTime);
+            if (transform.eulerAngles.z < 275.0 && transform.eulerAngles.z > 180.0)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 275.0f);
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * this.rotationSpeed * Time.deltaTime);
+            if (transform.eulerAngles.z > 85.0 && transform.eulerAngles.z < 180.0)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 85.0f);
+            }
+        }
+    }
+}
